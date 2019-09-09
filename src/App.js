58bc3a9e94363cc6bab1fe471e08da1e213b4470
Ben Orban <benjamin.orban@ehi.com>
2019-09-09 08:22:57 -0500

    Lesson 56

--- src/App.js
+++ src/App.js
@@ -13,17 +13,6 @@ class App extends Component {
         showPersons: false
     };
 
-    switchNameHandler = (newName) => {
-        this.setState({
-                persons: [
-                    {name: newName, age: 28},
-                    {name: 'Manu', age: 29},
-                    {name: 'Stephanie', age: 27},
-                ]
-            }
-        )
-    };
-
     nameChangedHandler = (event) => {
         this.setState({
                 persons: [
@@ -33,6 +22,12 @@ class App extends Component {
                 ]
             }
         )
+    };
+
+    deletePersonHandler = (personIndex) =>{
+        const persons = this.state.persons;
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
     };
 
     togglePersonsHandler = () => {
@@ -54,10 +49,11 @@ class App extends Component {
         if (this.state.showPersons) {
             persons = (
                 <div>
-                    {this.state.persons.map(person => {
+                    {this.state.persons.map((person, index) => {
                         return <Person
                             name={person.name}
                             age={person.age}
+                            click={() => this.deletePersonHandler(index)}
                         />
                     })}
                 </div>
