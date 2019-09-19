2cee797a0290b903c144a831b37d776fb46a21a3
Ben Orban <benjamin.orban@ehi.com>
2019-09-19 08:44:13 -0500

    Lesson 92

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Cockpit.module.css";
 
 const cockpit = props => {
+  useEffect(() => {
+    console.log("[Cockpit.js] usEffect");
+    // Http request
+  });
+
   const assignedClasses = [];
   let btnClass = "";
 
