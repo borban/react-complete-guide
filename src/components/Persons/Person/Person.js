f1016672165b9289a1a9536448aa1e93d0be3f85
Ben Orban <benjamin.orban@gmail.com>
2019-09-22 17:47:48 -0500

    Lesson 109

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,6 +4,15 @@ import classes from "./Person.css";
 import PropTypes from "prop-types";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log("[Person] rendering...");
     return (
@@ -14,6 +23,10 @@ class Person extends Component {
         <p key="i2">{this.props.children}</p>
         <input
           key="i3"
+          // ref={inputEl => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
